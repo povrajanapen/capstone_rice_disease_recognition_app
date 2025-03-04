8f3972e64e86200f6f457aa8d79fa7ba85765502
Sovansoupor <sovansouporeng@gmail.com>
2025-03-04 20:40:45 +0700

    Rice-000-FrontEnd-DateTime-util

--- lib/utils/date_time_util.dart
+++ lib/utils/date_time_util.dart
@@ -1,1 +1,18 @@
 // date time
+// lib/utils/date_utils.dart
+
+String getRelativeTime(DateTime dateTime) {
+  final now = DateTime.now();
+  final difference = now.difference(dateTime);
+
+  if (difference.inHours < 24) {
+    if (difference.inHours == 0) {
+      return 'Just now';
+    }
+    return '${difference.inHours}h ago';
+  } else if (difference.inDays < 7) {
+    return '${difference.inDays}d ago';
+  } else {
+    return '${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}';
+  }
+}
---
+++ lib/utils/scan_util.dart
@@ -0,0 +1,1 @@
+// Helper function to get the latest successful scan
