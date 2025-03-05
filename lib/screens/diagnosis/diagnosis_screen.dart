34f9ea79625786ba207af5f1a436ff515f8720c9
Sovansoupor <sovansouporeng@gmail.com>
2025-03-05 17:44:14 +0700

    Rice-004-FrontEnd-Implement-Diagnosis-screen

--- lib/screens/diagnosis/diagnosis_screen.dart
+++ lib/screens/diagnosis/diagnosis_screen.dart
@@ -1,7 +1,8 @@
+import 'package:capstone_dr_rice/models/diagnosis_model.dart';
 import 'package:flutter/material.dart';
 
 class DiagnosisScreen extends StatelessWidget {
-  const DiagnosisScreen({super.key});
+  const DiagnosisScreen({super.key, required DiagnosisModel diagnosis});
 
   @override
   Widget build(BuildContext context) {
