b420639a38df25a6b8662fcbb344d1b3e09ad3d6
Sovansoupor <sovansouporeng@gmail.com>
2025-03-21 00:09:44 +0700

    Rice-Update-Saved-screen

--- lib/screens/saved diagnosis/saved_diagnosis_screen.dart
+++ lib/screens/saved diagnosis/saved_diagnosis_screen.dart
@@ -5,14 +5,13 @@ import 'package:provider/provider.dart';
 import 'dart:io';
 import '../scan/result_screen.dart';
 
-
 class SaveScreen extends StatelessWidget {
   const SaveScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     // Access the saved diagnoses from provider
-    final savedDiagnoses =context.watch<DiagnosisProvider>().savedDiagnoses;
+    final savedDiagnoses = context.watch<DiagnosisProvider>().savedDiagnoses;
 
     return Scaffold(
       appBar: AppBar(
@@ -30,25 +29,31 @@ class SaveScreen extends StatelessWidget {
                 itemBuilder: (context, index) {
                   final diagnosis = savedDiagnoses[index];
 
-                  final String diagnosisId = diagnosis['id'] as String? ??
-                    DateTime.now().millisecondsSinceEpoch.toString();
+                  final String diagnosisId =
+                      diagnosis['id'] as String? ??
+                      DateTime.now().millisecondsSinceEpoch.toString();
                   //// Save disease tiles
                   return Container(
+                    //color: RiceColors.backgroundAccent,
+                    padding: EdgeInsets.symmetric(vertical: 8),
                     margin: const EdgeInsets.symmetric(
                       horizontal: RiceSpacings.s,
-                      vertical: RiceSpacings.s,
+                      vertical: RiceSpacings.l,
                     ),
                     decoration: BoxDecoration(
-                      color: RiceColors.neutralLight, 
+                      color: RiceColors.backgroundAccent,
                       borderRadius: BorderRadius.circular(RiceSpacings.s),
-                      border: Border.all(color: RiceColors.neutral),
+                      border: Border.all(color: RiceColors.neutral, width: 0.5),
                     ),
+
                     child: ListTile(
                       //// disease image
                       leading:
                           diagnosis['imagePath'] != null
                               ? ClipRRect(
-                                borderRadius: BorderRadius.circular(RiceSpacings.s),
+                                borderRadius: BorderRadius.circular(
+                                  RiceSpacings.s,
+                                ),
                                 child: Image.file(
                                   File(diagnosis['imagePath']),
                                   width: 85,
@@ -70,12 +75,15 @@ class SaveScreen extends StatelessWidget {
                               : const Icon(Icons.image_not_supported),
 
                       //// disease name
-                      title: Text(diagnosis['name'] ?? 'Unknown Disease', style: RiceTextStyles.button),
+                      title: Text(
+                        diagnosis['name'] ?? 'Unknown Disease',
+                        style: RiceTextStyles.button,
+                      ),
                       subtitle: Text(
                         'Accuracy: ${((diagnosis['accuracy'] ?? 0.0) * 100).toInt()}%',
                         style: RiceTextStyles.button.copyWith(
                           fontSize: 15,
-                          color: RiceColors.neutral
+                          color: RiceColors.neutral,
                         ),
                       ),
                       onTap: () {
@@ -91,7 +99,7 @@ class SaveScreen extends StatelessWidget {
                                       'name': diagnosis['name'],
                                       'description': diagnosis['description'],
                                       'accuracy': diagnosis['accuracy'],
-                                      'id': diagnosisId, 
+                                      'id': diagnosisId,
                                     },
                                   ),
                             ),
@@ -100,6 +108,7 @@ class SaveScreen extends StatelessWidget {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
                               content: Text('Image not available'),
+                              duration: Duration(milliseconds: 800),
                             ),
                           );
                         }
@@ -108,11 +117,14 @@ class SaveScreen extends StatelessWidget {
                         icon: const Icon(Icons.delete),
                         onPressed: () {
                           // Handle delete functionality
-                          context
-                              .read<DiagnosisProvider>()
-                              .removeDiagnosis(diagnosis['id']);
+                          context.read<DiagnosisProvider>().removeDiagnosis(
+                            diagnosis['id'],
+                          );
                           ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Diagnosis removed')),
+                            const SnackBar(content: Text('Diagnosis removed'),
+                            duration: Duration(milliseconds: 800),
+                            ),
+                            
                           );
                         },
                       ),
@@ -120,9 +132,6 @@ class SaveScreen extends StatelessWidget {
                   );
                 },
               ),
-              
     );
-    
   }
-  
 }
